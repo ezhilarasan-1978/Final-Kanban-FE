94f44213978447e06eedfc7171702d3f37e26291
priyanshunegi2398@gmail.com <priyanshunegi2398@gmail.com>
2023-05-30 13:53:04 +0530

    done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormArray } from '@angular/forms';
 import {CustomValidation} from '../service/CustomValidation';
 import { UserService } from '../service/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-register',
@@ -11,10 +12,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 
-export class RegisterComponent {
-  constructor(private userService:UserService, private fb:FormBuilder
+export class RegisterComponent implements OnInit{
+
+
+  constructor(private breakPoint: BreakpointObserver,private userService:UserService, private fb:FormBuilder
     , private matSnackBar:MatSnackBar, private routing: Router
     ) { }
+
+    DeskTopView:boolean=false;
+    ngOnInit(){
+      this.breakPoint.observe([Breakpoints.Handset]).subscribe(
+        result => {
+          this.DeskTopView = !result.matches;
+        }
+      )
+    }
+    
+    
 
     phonePattern = /^[7-9]\d{9}$/;
     // emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z]+\.[a-zA-Z]$/;
